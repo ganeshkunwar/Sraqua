724076c27f5f04257ec94ef1c54385625efd74cd
ganeshkunwar <ganeshkunwar1112@gmail.com>
2024-05-14 13:18:08 +0545

    EditProduct.js

---
+++ src/components/adminComponents/EditProduct.js
@@ -0,0 +1,169 @@
+import { React, useEffect, useState } from 'react';
+import axios from '../../api/axios';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const EDITPRODUCTS_URL = '/editProducts';
+
+const EditProduct = () => {
+
+    const { pId } = useParams();
+
+    const axiosPrivate = useAxiosPrivate();
+    const navigate = useNavigate();
+
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const [productName, setProductName] = useState('');
+    const [image, setImage] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState('');
+    const [stock, setStock] = useState('');
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        const getProduct = async () => {
+            try {
+                const response = await axios.get(`products/${pId}`, {
+                    signal: controller.signal
+                });
+                setProductName(response.data.productName);
+                setImage(response.data.image);
+                setCategory(response.data.category);
+                setPrice(response.data.price);
+                setStock(response.data.stock);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getProduct();
+    }, [pId]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axiosPrivate.put(EDITPRODUCTS_URL,
+                JSON.stringify({ id:pId, productName, image, category, price, stock }),
+                {
+                    headers: { 'Content-Type': 'application/json' }
+                }
+            );
+            setProductName('');
+            setImage('');
+            setCategory('');
+            setPrice('');
+            setStock('');
+            setSuccess(true);
+            setSuccessMsg('Product Edited Successfully');
+            navigate('/admin-pannel/dashboard');
+            setInterval(() => {
+                setSuccess(false);
+            }, 2000);
+        } catch (err) {
+            if (!err?.response) {
+                setErrorMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrorMsg('Missing Product Name, image, category, price or stock');
+            } else if (err.response?.status === 401) {
+                setErrorMsg('Unauthorized');
+            } else {
+                setErrorMsg("Failed to add product");
+            }
+            setError(true);
+            setInterval(() => {
+                setError(false);
+            }, 2000);
+        }
+    }
+
+    return (
+        <div className=' w-max h-max flex flex-col items-center justify-center my-5 max-lg:my-5 shadow-xl p-5'>
+            <div className={error ? 'absolute top-5 right-5 p-4 rounded-lg text-white bg-red-700 transition-all duration-300' : 'absolute -top-96 -right-96 p-4 rounded-lg text-white bg-red-700 transition-all duration-300'}>
+                {errorMsg}
+            </div>
+            <div className={success ? 'absolute top-5 right-5 p-4 rounded-lg text-white bg-green-700 transition-all duration-300' : 'absolute -top-96 -right-96 p-4 rounded-lg text-white bg-green-700 transition-all duration-300'}>
+                {successMsg}
+            </div>
+            <div className="text-3xl max-lg:text-3xl text-blue-950 font-bold mb-5 ">
+                Edit Prodcut
+            </div>
+
+            <div className="form">
+                <form action="POST" className="flex flex-col justify-center items-center" onSubmit={handleSubmit}>
+                    <div className="flex flex-col justify-center items-start m-3">
+                        <label htmlFor="name" className="text-base text-black my-2">
+                            Product name
+                        </label>
+                        <input
+                            type="text"
+                            placeholder='eg: Six Seven'
+                            className="w-60 bg-white shadow-xl focus:outline-none active:outline-none focus:border-white transition-colors duration-500 p-2 text-black"
+                            value={productName ?? ''}
+                            onChange={e => setProductName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center items-start m-3">
+                        <label htmlFor="image" className="text-base text-black my-2">
+                            Product image
+                        </label>
+                        <input
+                            type="text"
+                            placeholder='eg: https://i.imgur.com/wzN7TJr.jpg'
+                            className="w-60 bg-white shadow-xl focus:outline-none active:outline-none focus:border-white transition-colors duration-500 p-2 text-black"
+                            value={image ?? ''}
+                            onChange={e => setImage(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center items-start m-3 relative">
+                        <label htmlFor="category" className="text-base text-black my-2">
+                            Category
+                        </label>
+                        <input
+                            type='text'
+                            placeholder='eg: Fish Food'
+                            className="w-60 bg-white shadow-xl focus:outline-none active:outline-none focus:border-white transition-colors duration-500 p-2 text-black"
+                            value={category ?? ''}
+                            onChange={(e) => setCategory(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center items-start m-3 relative">
+                        <label htmlFor="price" className="text-base text-black my-2">
+                            Price
+                        </label>
+                        <input
+                            type='text'
+                            placeholder='eg: 600'
+                            className="w-60 bg-white shadow-xl focus:outline-none active:outline-none focus:border-white transition-colors duration-500 p-2 text-black"
+                            value={price ?? ''}
+                            onChange={(e) => setPrice(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center items-start m-3 relative">
+                        <label htmlFor="stock" className="text-base text-black my-2">
+                            Stock
+                        </label>
+                        <input
+                            type='text'
+                            placeholder='eg: 4'
+                            className="w-60 bg-white shadow-xl focus:outline-none active:outline-none focus:border-white transition-colors duration-500 p-2 text-black"
+                            value={stock ?? ''}
+                            onChange={(e) => setStock(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <input type='submit' className={"w-60 mt-7 bg-blue-950 text-white transition-all duration-500 p-3 cursor-pointer"} value={"Edit Product"} onClick={handleSubmit} />
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default EditProduct;
